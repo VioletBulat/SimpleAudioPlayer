6d418f93487cebd20498cba45a928f7e5b58f0ad
vitusencia <150796816+vitusencia@users.noreply.github.com>
2024-04-07 21:43:53 +0300

    Added volume control

--- src/SimpleAudioPlayer.java
+++ src/SimpleAudioPlayer.java
@@ -34,7 +34,7 @@ public class SimpleAudioPlayer {
 
             String command;
             do {
-                System.out.println("Commands: play, pause, resume, forward, rewind, stop, exit");
+                System.out.println("Commands: play, pause, resume, forward, rewind, increase volume, decrease volume, stop, exit");
                 command = scanner.nextLine();
 
                 switch (command) {
@@ -58,6 +58,31 @@ public class SimpleAudioPlayer {
                         break;
                     case "rewind":
                         clip.setFramePosition((int)Math.max(clip.getFramePosition() - clip.getFormat().getFrameRate() * 10, 0));
+                        break;
+                    case "increase volume":
+                        // FloatControl gainControl получает контроль громкости для аудиопотока с помощью метода getControl объекта clip
+                        // Тип контроля указывается как FloatControl.Type.MASTER_GAIN, что означает управление основной громкостью аудиопотока
+                        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+                        // Получает текущее значение громкости
+                        float currentGain = gainControl.getValue();
+                        // Получает максимально допустимое значение громкости
+                        float maximumGain = gainControl.getMaximum();
+                        // Значение, на которое будет увеличиваться громкость
+                        float increment = 2.0f;
+                        // Вычисляет новое значение громкости, которое является минимумом между текущим значением громкости
+                        // плюс инкрементом и максимально допустимым значением
+                        // Делается для предотвращения выхода за пределы допустимой громкости
+                        float newGain = Math.min(currentGain + increment, maximumGain);
+                        // Устанавливает новое значение громкости
+                        gainControl.setValue(newGain);
+                        break;
+                    case "decrease volume":
+                        FloatControl decreaseGainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+                        float currentDecreaseGain = decreaseGainControl.getValue();
+                        float minimumDecreaseGain = decreaseGainControl.getMinimum();
+                        float decrement = 2.0f;
+                        float newDecreaseGain = Math.max(currentDecreaseGain - decrement, minimumDecreaseGain);
+                        decreaseGainControl.setValue(newDecreaseGain);
                         break;
                     case "stop":
                         clip.stop();
